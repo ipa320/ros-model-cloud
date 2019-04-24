f5b9240490eb06b3e96650fa067f3976dde588d5
ipa-nhg <nhg@ipa.fhg.de>
2019-04-24 10:26:03 +0200

    add the setup of the workspace and th build of dependencies

--- setup_ws_depends.sh
+++ setup_ws_depends.sh
@@ -25,3 +25,16 @@ do
     ;;
     esac
 done
+
+cd ..
+
+catkin_make_isolated --only-pkg-with-deps $1 -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-3.8 --merge
+
+mkdir build && touch build/compile_commands.json
+cat build_isolated/*/compile_commands.json >> build/compile_commands.json
+sed -i -e 's/\]\[/\,/g' build/compile_commands.json
+
+source devel_isolated/setup.bash
+
+#roslaunch dump if needed
+#ros_model_extractor.py --package $1 --name $2 --launch --output true
